9a063d3d4f8cd055bf51fd21872b1e22643dd947
lazorfuzz <leontosy@gmail.com>
2019-06-13 18:32:53 -0700

    Fix team number

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ class Footer extends PureComponent {
   render() {
     return (
       <Wrapper>
-        <Text className="footerText">Made with <span role="img" aria-label="love">❤️</span> by IST440 Summer 2019 Team 3.</Text>
+        <Text className="footerText">Made with <span role="img" aria-label="love">❤️</span> by IST440 Summer 2019 Team 2.</Text>
         <Text className="footerText">View this project on <FooterLink target="_blank" href="https://github.com/lazorfuzz/game-of-decryption">GitHub.</FooterLink></Text>
       </Wrapper>
     );
@@ -23,6 +23,7 @@ const Wrapper = styled.div`
   align-items: center;
   position: relative;
   bottom: 0;
+  padding-bottom: 8px;
 `;
 
 const FooterLink = styled.a`
